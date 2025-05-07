8e5544a9758af39757743c559281ebe6beda1749
luofamn1 <luofamn1@yonyou.com>
2025-05-07 14:00:25 +0800

    feature/rebase-test

--- src/main/java/org/example/test.java
+++ src/main/java/org/example/test.java
@@ -3,5 +3,6 @@ package org.example;
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello Demo!");
+        System.out.println("rebase-test");
     }
 }
