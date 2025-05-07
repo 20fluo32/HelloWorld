764a14e408ea94b4b6d15607fae90ecff97f1b38
luofamn1 <luofamn1@yonyou.com>
2025-05-07 14:15:26 +0800

    rebase

--- src/main/java/org/example/test.java
+++ src/main/java/org/example/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hello Demo!");
         System.out.println("rebase-test");
+        System.out.println("rebase!");
     }
 }
